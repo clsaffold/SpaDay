ca7d7cffea40b721e73816ec0208529a40a9e76f
csaffold <ceciliasaffold@gmail.com>
2020-11-17 19:05:36 -0600

    commit from last week

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [Route("user/add")]
+        public IActionResult Index()
+        {
+            Events.Add(new Event(name));
+            
+            return View();
+        }
+    }
+}
